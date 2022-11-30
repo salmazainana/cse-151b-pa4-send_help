6b6ac1f81b6a32785e84ad0f633a8dec4712d868
hieuluu <hieuhuynhluu@ucsd.edu>
2022-11-29 17:19:06 -0800

    SIMCLR

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ def supcon_train(args, model, datasets, tokenizer):
             
             features = torch.cat([logits_pos.unsqueeze(1), logits_neg.unsqueeze(1)], dim=1)
         
-            loss = criterion.forward(features, labels)
+            loss = criterion.forward(features)
             
             loss.backward()
             model.optimizer.step()  # backprop to update the weights
@@ -154,7 +154,7 @@ def test(args, model, datasets, tokenizer, split='test'):
     print(embedding)
     image = umap.plot.points(embedding, labels=labe)
     figure = image.get_figure()
-    figure.savefig('img2')
+    figure.savefig('SimCLR')
     #plt.close(figure)
 
                 
